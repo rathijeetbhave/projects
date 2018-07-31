48b34b1a96bb03aa6197a4739e2ced08e8c3195e
Rathijeet Bhave <rathijeet.bhave@informds.com>
2018-07-31 22:42:07 +0530

    modified wsgi to work eith django

--- chat-system/backend/chatApp/wsgi.py
+++ chat-system/backend/chatApp/wsgi.py
@@ -7,9 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/Users/rathijeetbhave/programs/projects/chat-system/backend/chatApp')
+sys.path.append('/Users/rathijeetbhave/programs/projects/chat-system/backend/venv/lib/python2.7/site-packages/')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatApp.settings")
 
